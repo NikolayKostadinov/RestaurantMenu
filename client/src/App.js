25aef4b67543109826e8a2fe60a290c8d6a8e56b
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2023-04-01 20:33:47 +0300

    refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import { Route, Routes } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
-import { AlertProvider } from './contexts/AlertContext';
-import { RestaurantManagementContextProvider } from './contexts/RestaurantManagementContext';
+import {Route, Routes} from 'react-router-dom';
+import {AuthProvider} from './contexts/AuthContext';
+import {AlertProvider} from './contexts/AlertContext';
+import {RestaurantManagementContextProvider} from './contexts/RestaurantManagementContext';
 
 import MainNavigation from './components/main-nav/MainNavigation';
 import Footer from './components/footer/Footer';
@@ -15,37 +15,37 @@ import RestaurantManagement from './components/restaurant-management/RestaurantM
 import Reservation from './components/reservation/Reservation';
 import Alert from './components/common/alert/Alert';
 import Spinner from './components/common/spinner/Spinner';
-
-import './App.css';
 import ReservationManagement from './components/reservation/reservation-management/ReservationManagement';
 
+import './App.css';
+
 function App() {
-  return (
-    <AlertProvider>
-      <AuthProvider>
-        <div className="App">
-          <MainNavigation />
-          <RestaurantManagementContextProvider>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/reservation/:restaurantId/:title" element={<Reservation />} />
-              <Route path="/menu/:restaurantId" element={<Menu />} />
-              <Route element={<Authenticated/>}>
-                <Route path="/logout" element={<Logout />} />
-                <Route path="/management" element={<RestaurantManagement />} />
-                <Route path="/reservations" element={<ReservationManagement />} />
-              </Route>
-            </Routes>
-          </RestaurantManagementContextProvider>
-          <Footer />
-        </div>
-        <Spinner />
-        <Alert />
-      </AuthProvider>
-    </AlertProvider>
-  );
+    return (
+        <AlertProvider>
+            <AuthProvider>
+                <div className="App">
+                    <MainNavigation/>
+                    <RestaurantManagementContextProvider>
+                        <Routes>
+                            <Route path="/" element={<Home/>}/>
+                            <Route path="/login" element={<Login/>}/>
+                            <Route path="/register" element={<Register/>}/>
+                            <Route path="/reservation/:restaurantId/:title" element={<Reservation/>}/>
+                            <Route path="/menu/:restaurantId" element={<Menu/>}/>
+                            <Route element={<Authenticated/>}>
+                                <Route path="/logout" element={<Logout/>}/>
+                                <Route path="/management" element={<RestaurantManagement/>}/>
+                                <Route path="/reservations" element={<ReservationManagement/>}/>
+                            </Route>
+                        </Routes>
+                    </RestaurantManagementContextProvider>
+                    <Footer/>
+                </div>
+                <Spinner/>
+                <Alert/>
+            </AuthProvider>
+        </AlertProvider>
+    );
 }
 
 export default App;
