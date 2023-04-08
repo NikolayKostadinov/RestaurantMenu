b9b34e031b738405c82593ba1b1ce7af2592d830
NikolayKostadinov <Kostadinov.Nikolay.D@gmail.com>
2023-04-08 12:08:36 +0300

    logout bug fix

--- client/src/components/logout/Logout.js
+++ client/src/components/logout/Logout.js
@@ -1,12 +1,11 @@
-import{useContext} from "react";
 import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../../contexts/AuthContext";
+import {useAuthContext} from "../../contexts/AuthContext";
 
 import * as authService from "../../services/authService";
 
 const Logout = () => {
     const navigate = useNavigate();
-    const {user, userLogout} = useContext(AuthContext);
+    const {user, userLogout} = useAuthContext();
     authService.logout(user)
     .then(()=>{
         userLogout();
--- client/src/services/utils/users_store.js
+++ client/src/services/utils/users_store.js
@@ -3,6 +3,6 @@ const STORE_KEY = 'userData';
 const getUserData = () => JSON.parse(localStorage.getItem(STORE_KEY));
 const setUserData = (userData) => localStorage.setItem( STORE_KEY, JSON.stringify(userData));
 const clearUserData = () => localStorage.removeItem(STORE_KEY);
-const isAuthenticated = () => getUserData() !== null;
+const isAuthenticated = () => !!getUserData()?.username;
 
 export { clearUserData, getUserData, setUserData, isAuthenticated }
